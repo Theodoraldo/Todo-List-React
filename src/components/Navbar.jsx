01081cf8e17387d07051e812304f3eacbba67084
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-08 04:20:53 +0000

    Add navigation bar to app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdown && ref.current && !ref.current.contains(event.target)) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+  }, [dropdown]);
+
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li ref={ref}>
+          <button onClick={() => setDropdown(!dropdown)}>
+            Services <span>&#8595;</span>
+          </button>
+          {dropdown && (
+            <ul>
+              <li>Design</li>
+              <li>Development</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
